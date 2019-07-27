462c4621316552262cd3938e8b11809ac3f531c6
steview-d <dave.laffan@outlook.com>
2019-07-27 17:34:57 +0000

    Add preload of images

--- js/game.js
+++ js/game.js
@@ -2,16 +2,21 @@ class SceneMain extends Phaser.Scene {
     constructor() {
         super('game');
     }
-    preload() 
-    {
-        this.load.image('archery', 'images/archery.jpg');
+    preload() {
+        var image_list = ['archery', 'baseball', 'basketball', 'canoeing', 'climbing', 'cricket', 'cycling',
+                          'fishing', 'football', 'golf', 'kayaking', 'lacrosse', 'nfl', 'pool', 'running',
+                          'showjumping', 'skateboarding', 'surfing', 'swimming', 'tennis', 'volleyball',
+                          'wakeboarding', 'waterpolo', 'weightlifting', 'whitewaterrafting', 'windsurfing', 'yoga']
+        var i;
+        for (i = 0; i < image_list.length; i++) {
+            this.load.image(image_list[i], 'images/'+image_list[i]+'.jpg');
+        }
+
     }
-    create() 
-    {
-        this.add.image(400, 300, 'archery');
+    create() {
+        this.add.image(400, 300, 'weightlifting');
     }
-    update() 
-    {
-        
+    update() {
+
     }
 }
